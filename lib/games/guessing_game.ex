6d3c13d18085f660af18b1753370b03a3d988953
Ravi Kumar <kumarravi1165@gmail.com>
2023-10-01 23:32:36 +0530

    finish Games: Guessing Game exercise

---
+++ lib/games/guessing_game.ex
@@ -0,0 +1,33 @@
+defmodule Games.GuessingGame do
+  def play() do
+    guess = IO.gets("Guess a number between 1 and 10: ") |> String.trim() |> String.to_integer()
+    answer = Enum.random(1..10)
+
+    if guess == answer do
+      IO.puts("You win!")
+    else
+      IO.puts("Incorrect!")
+      play_helper(answer, 5)
+    end
+  end
+
+  def play_helper(answer, attempts) do
+    guess = IO.gets("Enter your guess: ") |> String.trim() |> String.to_integer()
+
+    cond do
+      guess == answer ->
+        IO.puts("Correct!")
+
+      attempts == 0 ->
+        IO.puts("You lose! the answer was #{answer}")
+
+      attempts > 0 and guess < answer ->
+        IO.puts("Too Low!")
+        play_helper(answer, attempts - 1)
+
+      attempts > 0 and guess > answer ->
+        IO.puts("Too High!")
+        play_helper(answer, attempts - 1)
+    end
+  end
+end
