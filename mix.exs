519eab58cb51396552245183dc37706869ff7bd2
Ravi Kumar <kumarravi1165@gmail.com>
2023-10-14 10:10:53 +0530

    added monitoring applications

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Games.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :observer, :wx, :runtime_tools],
       mod: {Games.Application, []}
     ]
   end
